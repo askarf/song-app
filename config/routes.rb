63b0e7d12037a60bf1fd2427f771d75e747ac9f2
askarf <ayalakarf@gmail.com>
2023-10-26 14:37:38 -0400

    added update controller and path

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get "/songs/:id" => "songs#show"
   post "/songs" => "songs#create"
   delete "/songs/:id" => "songs#destroy"
+  patch "songs/:id" => "songs#update"
 end
