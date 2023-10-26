99b549cdf3246a6c879c2259f1ad685d1b257e7e
askarf <ayalakarf@gmail.com>
2023-10-26 14:17:51 -0400

    added route and controller to delete a song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,12 +9,18 @@ class SongsController < ApplicationController
     render :show
   end
 
+  def destroy
+    song = Song.find_by(id: params["id"])
+    song.delete
+    render json: { message: "song deleted" }
+  end
+
   def create
     @song = Song.create(
-      title: "Heros",
-      album: "Miracles II",
-      artist: "Chayala Neuhaus",
-      year: 2018,
+      title: params["title"],
+      album: params["album"],
+      artist: params["artist"],
+      year: params["year"],
     )
     render json: { message: "new instance created" }
   end
